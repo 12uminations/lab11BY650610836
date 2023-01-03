907ac02981972a0b2030d1627712bf12469dd54d
12uminatons <nawatpokemon@gmail.com>
2023-01-03 16:15:46 +0700

    lab11 done

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,40 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include<fstream>
+#include <iomanip>
+#include<string>
+#include<cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ifstream source;
+	source.open("score.txt");
+	string textline;
+    float i=0;
+    float sum=0;
+    float sig=0;
+    float x;
+    float sd;
+    float mean;
+	while(getline(source,textline)){
+		sum += atof(textline.c_str());
+        i++;
+	}
+    mean = sum/i;
+     source.close();
+    source.open("score.txt");
+    string textline2;
+    while(getline(source,textline2)){
+        x = mean - atof(textline2.c_str());
+		sig =sig +pow(x,2);
+	}
+	sd= sqrt(sig/i);
+    source.close();
+
+    cout << "Number of data = "<<i<<"\n";
+    cout << setprecision(3);
+    cout << "Mean = "<<mean<<"\n";
+    cout << "Standard deviation = "<<sd;
+    return 0;
+}
